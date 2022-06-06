3f79c9597088e45ef7e0b5adcc4ddb917adf4f48
ricardosacadura <ricardorochasacadura@gmail.com>
2022-06-06 19:17:18 +0100

    project 1.0

--- js/rorschach.js
+++ js/rorschach.js
@@ -6,31 +6,31 @@
 class Rorschach {
     
     
-    constructor(rMin, rMax, oscMin, oscMax, hue) {
+    constructor(radius_min, radius_max, oscMin, oscMax, hue) {
 
         this.transX = width_canvas / 2;
         this.transY = height_canvas / 2;
         this.xoff = map(int(random(1, 5)), 1, 5, 1000, 5000);
         this.yoff = 0;
-        this.rMin = rMin;
-        this.rMax = rMax;
+        this.radius_min = radius_min;
+        this.radius_max = radius_max;
         this.oscMin = oscMin;
         this.oscMax = oscMax;
         this.hue = hue;
     }
 
     drawRorschach() {
-
+        
         push();
         fill(this.hue);
-        let osc2 = map(sin(this.yoff), -1, 1, this.oscMin, this.oscMax);
+        let oscilation = map(sin(this.yoff), -1, 1, this.oscMin, this.oscMax);
 
-        translate(this.transX, this.transY + osc2);
+        translate(this.transX, this.transY + oscilation);
 
         beginShape();
         for (var a = -PI / 2; a <= (3 * PI) / 2; a += da) {
             let n = noise(this.xoff, this.yoff); // -> geting me values from 0 and 1;
-            let r = map(n, 0, 1, this.rMin, this.rMax + osc2);
+            let r = map(n, 0, 1, this.radius_min, this.radius_max + oscilation);
             if (a <= PI / 2) { // -> left wing
                 this.xoff += dx; // -> scrubbing through perlinNoise "timeline"
             } else { // -> right wind
